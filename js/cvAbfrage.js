c3f1ad9a94c94c89dad17101375f0bb6e093fe60
alex <enna@mailbox.org>
2023-05-02 19:26:18 +0200

    Parsen von Zeit

--- js/cvAbfrage.js
+++ js/cvAbfrage.js
@@ -1,5 +1,42 @@
 import {cvRequest} from "./openai/openai.mjs";
+import {output_cvprompt} from "./openai/prompts.mjs";
+import {mylog, parseGermanDateString} from "./util/common.mjs";
+
+const date1 = new Date("2023-05-02");
+const date2 = new Date("2023-05-10");
+
+const differenceInMilliseconds = date2 - date1;
+
+// Konvertiere Millisekunden in andere Einheiten
+const differenceInSeconds = differenceInMilliseconds / 1000;
+const differenceInMinutes = differenceInSeconds / 60;
+const differenceInHours = differenceInMinutes / 60;
+const differenceInDays = differenceInHours / 24;
+
+console.log("Zeitunterschied in Millisekunden:", differenceInMilliseconds);
+console.log("Zeitunterschied in Sekunden:", differenceInSeconds);
+console.log("Zeitunterschied in Minuten:", differenceInMinutes);
+console.log("Zeitunterschied in Stunden:", differenceInHours);
+console.log("Zeitunterschied in Tagen:", differenceInDays);
 
 
-await cvRequest(`
-Was hast Du Ende 2008 gemacht?`)
+
+function monthDiff(date1, date2) {
+    let months;
+    months = (date2.getFullYear() - date1.getFullYear()) * 12;
+    months -= date1.getMonth();
+    months += date2.getMonth();
+
+    // Falls date2 kleiner ist als date1 wird ein negativer Wert zurückgegeben
+    if (date2.getDate() < date1.getDate()) {
+        months--;
+    }
+
+    return months;
+}
+
+const date1 = new Date("2023-05-02");
+const date2 = new Date("2023-10-01");
+
+const differenceInMonths = monthDiff(date1, date2);
+console.log("Zeitunterschied in Monaten:", differenceInMonths);
--- js/util/common.mjs
+++ js/util/common.mjs
@@ -16,6 +16,12 @@ function getCurrentDate_ddMMYYYY() {
     return `${day}.${month}.${year}`;
 }
 
+// Benutzerdefinierte zipWithIndex-Funktion
+function zipWithIndex(array) {
+    return array.map((element, index) => [element, index]);
+}
+
+
 function mylog(s) {
     console.log(s);
 }
@@ -24,5 +30,94 @@ function myerror(s) {
     console.error(s);
 }
 
+function parseGermanDateString(dateString) {
+    const [day, month, year] = dateString.split('.');
+    const parsedDate = new Date(year, month - 1, day);
+    return parsedDate;
+}
 
-export {getCurrentTime, getCurrentDate_ddMMYYYY, mylog, myerror};
+
+function milliSecondsBetween(date1, date2) {
+    const differenceInMilliseconds = date2 - date1;
+    return differenceInMilliseconds;
+}
+
+function secondsBetween(date1, date2) {
+    const differenceInMilliseconds = date2 - date1;
+    const differenceInSeconds = differenceInMilliseconds / 1000;
+    return differenceInSeconds;
+}
+
+function minutesBetween(date1, date2) {
+    const differenceInMilliseconds = date2 - date1;
+    const differenceInSeconds = differenceInMilliseconds / 1000;
+    const differenceInMinutes = differenceInSeconds / 60;
+    return differenceInMinutes;
+}
+
+function hoursBetween(date1, date2) {
+    const differenceInMilliseconds = date2 - date1;
+    const differenceInSeconds = differenceInMilliseconds / 1000;
+    const differenceInMinutes = differenceInSeconds / 60;
+    const differenceInHours = differenceInMinutes / 60;
+    return differenceInHours;
+}
+
+function daysBetween(date1, date2) {
+    const differenceInMilliseconds = date2 - date1;
+    const differenceInSeconds = differenceInMilliseconds / 1000;
+    const differenceInMinutes = differenceInSeconds / 60;
+    const differenceInHours = differenceInMinutes / 60;
+    const differenceInDays = differenceInHours / 24;
+    return differenceInDays;
+}
+
+
+/*
+Beachte, dass diese Methode nur die Monatsdifferenz schätzt, ohne die verbleibenden Tage zu berücksichtigen.
+ Wenn du eine genauere Berechnung benötigst, kannst du den Zeitunterschied in Tagen berechnen und dann durch
+ die durchschnittliche Anzahl von Tagen pro Monat teilen (z.B. 30,44 Tage).
+ Beachte jedoch, dass dieser Ansatz auch nicht perfekt ist, da er die unterschiedliche Anzahl
+  von Tagen in verschiedenen Monaten nicht berücksichtigt.
+ */
+function monthsBetween(date1, date2) {
+    let yearsDifference = date2.getFullYear() - date1.getFullYear();
+    let monthsDifference = date2.getMonth() - date1.getMonth();
+
+// Berechne die Gesamtdifferenz in Monaten
+    let totalMonthsDifference = yearsDifference * 12 + monthsDifference;
+
+    return totalMonthsDifference;
+
+}
+
+function yearsBetween(date1, date2) {
+    // Berechne die Differenz in Millisekunden
+    let differenceInMilliseconds = date2 - date1;
+
+// Konvertiere die Differenz in Tage
+    let differenceInDays = differenceInMilliseconds / (1000 * 60 * 60 * 24);
+
+// Konvertiere die Differenz in Jahre
+    let differenceInYears = differenceInDays / 365.25;
+    return differenceInYears;
+
+}
+
+
+export {
+    getCurrentTime,
+    getCurrentDate_ddMMYYYY,
+    mylog,
+    myerror,
+    zipWithIndex,
+    parseGermanDateString,
+    milliSecondsBetween,
+    secondsBetween,
+    minutesBetween,
+    hoursBetween,
+    daysBetween,
+    monthsBetween,
+    yearsBetween
+
+};

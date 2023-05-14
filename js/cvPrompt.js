618dd733d3566b38bd54962e840126444a4f9f17
alex <enna@mailbox.org>
2023-05-14 19:26:46 +0200

    Prompt für inneren Dialog.

--- js/cvPrompt.js
+++ js/cvPrompt.js
@@ -1,4 +1,4 @@
 import cv from "./cv/cv.mjs";
 import common from "./util/common.mjs"
 
-common.llog.mylog(await cv.cvprompt(`Schreibe eine kurze Lebenslauf-Zusammenfassung.`,false));
+common.llog.mylog(await cv.cvprompt(``,false));

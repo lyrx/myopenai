38b35a7c6bee673b2d05f832298e6291cfb76839
alex <enna@mailbox.org>
2023-05-15 15:37:46 +0200

    cv prompt

--- js/cvPrompt.js
+++ js/cvPrompt.js
@@ -1,4 +1,4 @@
 import cv from "./cv/cv.mjs";
 import common from "./util/common.mjs"
 
-common.llog.mylog(await cv.cvprompt(``,false));
+common.llog.mylog(await cv.cvprompt(`Schreibe eine Kurzbeschreibung Deiner Person in einem Satz.`,false));
